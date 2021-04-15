c62f465c9ad00a4d1f8a3e077468cf42bb8d07bb
aulkiller <58225261+aulkiller@users.noreply.github.com>
2021-04-15 15:03:43 +0700

    db-connected

--- persistence context.cs
+++ persistence context.cs
@@ -1,7 +1,9 @@
-﻿using System.Data.Entity;
+﻿using MySql.Data.EntityFramework;
+using System.Data.Entity;
 
 namespace LatihanMVVM
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     class LatihanContext : DbContext
     {
         public DbSet<ItemPenjualan> DaftarItemPenjualan { get; set; }
